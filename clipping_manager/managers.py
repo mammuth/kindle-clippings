20043498548a00caf3756b81abc693467df95fe5
Max Muth <kontakt@maxi-muth.de>
2019-06-24 19:30:24 +0200

    Fix random quote page if there is no imported clipping

--- clipping_manager/managers.py
+++ clipping_manager/managers.py
@@ -3,16 +3,21 @@ from random import randint
 from django.db import models
 from django.db.models import Count
 
+from clipping_manager.models import Clipping
+
 
 class ClippingQuerySetManager(models.QuerySet):
 
     def for_user(self, user):
         return self.filter(user=user)
 
-    def random(self):
+    def random(self) -> Clipping:
         count = self.aggregate(count=Count('id'))['count']
         random_index = randint(0, count - 1)
-        return self.all()[random_index]
+        try:
+            clip = self.all()[random_index]
+        except ValueError:
+            return self.none()
 
 
 class BookQuerySetManager(models.QuerySet):
