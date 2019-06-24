b357afb4db50c753c01e784a663ae80f51869dfa
Max Muth <kontakt@maxi-muth.de>
2019-06-24 19:38:00 +0200

    Fix typing import error

--- clipping_manager/managers.py
+++ clipping_manager/managers.py
@@ -3,15 +3,13 @@ from random import randint
 from django.db import models
 from django.db.models import Count
 
-from clipping_manager.models import Clipping
-
 
 class ClippingQuerySetManager(models.QuerySet):
 
     def for_user(self, user):
         return self.filter(user=user)
 
-    def random(self) -> Clipping:
+    def random(self):
         count = self.aggregate(count=Count('id'))['count']
         random_index = randint(0, count - 1)
         try:
