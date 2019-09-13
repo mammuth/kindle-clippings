0e68b5d69fb22e7a496a28339a2034323f654471
Max Muth <kontakt@maxi-muth.de>
2019-09-13 17:20:32 +0200

    Add date_joined and clippings count to user admin

--- clipping_manager/admin.py
+++ clipping_manager/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from django.db import models
 
 from clipping_manager.models import Clipping, Book, EmailDelivery
 
@@ -19,3 +22,24 @@ class EmailDeliveryAdmin(admin.ModelAdmin):
     search_fields = ('user', 'user__email')
     list_filter = ('user', )
     list_display = ('user', 'interval', 'last_delivery', )
+
+
+# Custom UserAdmin
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'date_joined', 'get_num_clippings', )
+    list_filter = ('date_joined', )
+    ordering = ('-date_joined', )
+
+    def get_queryset(self, request):
+        qs = super(UserAdmin, self).get_queryset(request)
+        qs = qs.annotate(models.Count('clippings'))
+        return qs
+
+    def get_num_clippings(self, obj):
+        return obj.clippings__count
+    get_num_clippings.short_description = 'Number of clippings'
+    get_num_clippings.admin_order_field = 'clippings__count'
+
+
+admin.site.unregister(get_user_model())
+admin.site.register(get_user_model(), UserAdmin)
