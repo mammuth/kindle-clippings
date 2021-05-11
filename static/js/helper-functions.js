1d93545c9a6399f8901911299cde5b3b83e28cb6
Jakub Serwatka <kubaserw@gmail.com>
2021-05-11 17:55:28 +0200

    refactor: move book.html js to helper-functions.js

--- static/js/helper-functions.js
+++ static/js/helper-functions.js
@@ -1,3 +1,4 @@
+// ------- browse.html -------
 // Highlights the searched phrase ('Content contains') in clippings 
 function highlightClippings(wordToHighlight) {
     // If empty -> stop the function
@@ -15,4 +16,52 @@ function highlightClippings(wordToHighlight) {
 
         clipping.innerHTML = highlightedClipping;
     });
+}
+
+// ------- books.html -------
+// Hides books which doesn't match the search bar value
+function searchBook() {
+    const searchInput = document.querySelector('#search-book');
+    const books = document.querySelectorAll('.book-element');
+
+    function displayMatches() {
+        const regex = new RegExp(this.value, 'gi');
+
+        books.forEach(book => {
+            const title = book.querySelector('.book-title').textContent;
+            const author = book.querySelector('.book-author').textContent;
+
+            if (title.match(regex) || author.match(regex)) {
+                book.style.display = 'block';
+            }
+            else {
+                book.style.display = 'none';
+            }
+        })
+    }
+
+    searchInput.addEventListener('keyup', displayMatches);
+}
+
+// Switches between the list and the gallery book view + controls buttons' colors
+function switchView() {
+    const galleryBtn = document.querySelector('#btn-gallery');
+    const listBtn = document.querySelector('#btn-list');
+    const bookView = document.querySelector('#book-view');
+
+    function viewControl(viewToSet) {
+        buttonToDisable = (this === galleryBtn) ? listBtn : galleryBtn;
+
+        // Swap buttons' colors
+        buttonToDisable.classList.remove('btn-dark');
+        this.classList.remove('btn-light');
+        this.classList.add('btn-dark');
+
+        // Switch the book view
+        bookView.className = (this === galleryBtn) ? 'gallery-view' : 'list-view';
+    }
+
+    galleryBtn.addEventListener('click', viewControl);
+    listBtn.addEventListener('click', viewControl);
+
 }
