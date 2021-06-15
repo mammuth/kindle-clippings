b86ab646cbc1cff8b90c77d36a5e595d3b3c02d7
Jakub Serwatka <kubaserw@gmail.com>
2021-06-15 11:12:54 +0200

    refactor: move book views functions to a seperate file

---
+++ static/js/components.js
@@ -0,0 +1,49 @@
+// ------- books.html -------
+const booksView = {
+    // Hides books which doesn't match the search bar value
+    searchBook() {
+        const searchInput = document.querySelector('#search-book');
+        const books = document.querySelectorAll('.book-element');
+    
+        function displayMatches() {
+            const regex = new RegExp(this.value, 'gi');
+    
+            books.forEach(book => {
+                const title = book.querySelector('.book-title').textContent;
+                const author = book.querySelector('.book-author').textContent;
+    
+                if (title.match(regex) || author.match(regex)) {
+                    book.classList.remove('hidden');
+                }
+                else {
+                    book.classList.add('hidden');
+                }
+            })
+        }
+    
+        searchInput.addEventListener('keyup', displayMatches);
+    },
+    // Switches between the list and the gallery book view + controls buttons' colors
+    switchView() {
+        const galleryBtn = document.querySelector('#btn-gallery');
+        const listBtn = document.querySelector('#btn-list');
+        const bookView = document.querySelector('#book-view');
+    
+        function viewControl() {
+            buttonToDisable = (this === galleryBtn) ? listBtn : galleryBtn;
+    
+            // Swap buttons' colors
+            buttonToDisable.classList.remove('btn-dark');
+            buttonToDisable.classList.add('btn-light');
+    
+            this.classList.remove('btn-light');
+            this.classList.add('btn-dark');
+    
+            // Switch the book view
+            bookView.className = (this === galleryBtn) ? 'gallery-view' : 'list-view';
+        }
+    
+        galleryBtn.addEventListener('click', viewControl);
+        listBtn.addEventListener('click', viewControl);
+    }
+}
