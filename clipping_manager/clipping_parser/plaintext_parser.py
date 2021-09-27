2c7d5e1e2ae66b027ec4d331869b21657720442d
Jakub Serwatka <kubaserw@gmail.com>
2021-09-27 13:45:26 +0200

    fix: plaintext import doesn't support Windows eol char

--- clipping_manager/clipping_parser/plaintext_parser.py
+++ clipping_manager/clipping_parser/plaintext_parser.py
@@ -1,9 +1,13 @@
 from typing import Union, Dict, List
 
+from httpagentparser import detect
 
-def get_clips_from_text(content: Union[str, bytes]) -> List[str]:
+def get_clips_from_text(content: Union[str, bytes], http_agent: str) -> List[str]:
     if isinstance(content, bytes):
         content = content.decode('utf-8')
 
-    clips = [c.strip() for c in content.split('\n\n') if c.strip() != '']
+    user_os_name = detect(http_agent)['os']['name']
+    eol_seperator = '\r\n' if user_os_name == 'Windows' else '\n\n'
+
+    clips = [c.strip() for c in content.split(eol_seperator*2) if c.strip() != '']
     return clips
